a8511eaa8cefea8526e49edaa2dc365997682db0
刘忻沂 <liuxy@enveesoft.com>
2014-09-09 18:12:59 +0800

    review update

--- createjs_games_NYP/FirstGame/js/gameClass.js
+++ createjs_games_NYP/FirstGame/js/gameClass.js
@@ -1,28 +1,27 @@
 
-goog.provide("root.gameClass");
-goog.require("org.createjs.easeljs.SoundJS");
-goog.require("bok.apps.preloader.CanvasPreloaderApp");
 
 
-imgContainer = [];
-function gameClass(stage) 
+function GameClass(stage, imgContainer)
 {
 	
 	console.log("hi!");
+    this.stage_ = stage;
 	this.bg = new createjs.Bitmap(imgContainer["imgs/bg.png"]);
 	this.red = new createjs.Bitmap(imgContainer["imgs/red.png"]);
 	this.blue = new createjs.Bitmap(imgContainer["imgs/blue.png"]);
-	
-	this.LoadImage =  function()
-	 {
-		 this.blue.y = 150;
-		 stage.addChild(this.bg);
-		 stage.addChild(this.red);
-		 stage.addChild(this.blue);
-	 }
-	 
-	 this.OnMouseclick = function(e)
-	 {
-		 console.log('clicked!')
-	 }
 }
+
+GameClass.prototype.loadImage = function() {
+    this.blue.y = 150;
+    this.stage_.addChild(this.bg);
+    this.stage_.addChild(this.red);
+    this.stage_.addChild(this.blue);
+
+};
+
+GameClass.prototype.onMouseClick = function(e) {
+    console.log('clicked!');
+};
+GameClass.prototype.start = function() {
+    this.loadImage();
+};
