6929b170e9d3423f8be5910946289883793cda3b
huanzhz <huanzhangrain@gmail.com>
2014-09-10 17:54:49 +0800

    a4

--- createjs_games_NYP/FirstGame/js/Creep.js
+++ createjs_games_NYP/FirstGame/js/Creep.js
@@ -12,18 +12,39 @@ BOK.inherits(Creep, createjs.Container);
 /**
  * @constructor
  * */
-function Creep(speed) {
+function Creep(speed, alive) {
     //this line is a must-have in prototype-chain style inheritance
     //Compare to JAVA this works as super();
-	
+
     createjs.Container.call(this);
 	this.speed_ = speed;
+	this.alive_ = alive;
 
     //we store the reference of creep img in a member varibale so it can be accessed later
     this.creepImg_ = new createjs.Bitmap(imgContainer["imgs/red.png"]);
+	this.creepImg_.scaleX = 0.5;
+	this.creepImg_.scaleY = 0.5;
     this.addChild(this.creepImg_);
+	
+	
 }
 
 Creep.prototype.move =  function() {
 	this.x = this.x + this.speed_;
 };
+
+Creep.prototype.destory = function(e) {
+	this.alive_ = 1;
+};
+
+
+Creep.prototype.remove = function(){
+
+	if(this.alive_ == 1){
+		console.log("y!");
+		return true;
+	}else{
+		return false;
+	}
+};
+
--- createjs_games_NYP/FirstGame/js/gameClass.js
+++ createjs_games_NYP/FirstGame/js/gameClass.js
@@ -1,33 +1,51 @@
 function GameClass(stage, imgContainer)
 {
-	
 	console.log("hi!");
     this.stage_ = stage;
 	this.bg = new createjs.Bitmap(imgContainer["imgs/bg.png"]);
-	this.red = new Creep(1);
+	this.red = [];
+	this.red.push(new Creep(1, 0));
+	this.red.push(new Creep(0.1, 0));
 	this.blue = new BlueCreep(2);
-
-   // this.bg.addEventListener('click', this.onMouseClick);
 	
-
+	for( var i = 0; i < this.red.length; i++){
+		this.red[i].addEventListener('click', Delegate.create(this.red[i],this.red[i].destory));
+	}
 }
 
 GameClass.prototype.tick = function(event) {
-   this.red.move();
+	for( var i = 0; i < this.red.length; i++){
+			this.red[i].move();
+			
+			if(this.red[i].remove()){
+				console.log("e!");
+				if(this.stage_.removeChild(this.red[i])){
+					console.log("s!");
+					this.red.splice(i,1);
+				}
+			}
+	}
    this.blue.move();
 };
 
+			
 
 GameClass.prototype.loadImage = function() {
     this.blue.y = 150;
     this.stage_.addChild(this.bg);
-    this.stage_.addChild(this.red);
+	for( var i = 0; i < this.red.length; i++){
+		this.stage_.addChild(this.red[i]);
+	}
     this.stage_.addChild(this.blue);
 
 };
 
 GameClass.prototype.onMouseClick = function(e) {
     console.log('stage clicked!');
+	/*if(this.stage_.removeChild(this.red)){
+		this.stage_.addChild(this.red);
+		this.red.x = 0;
+	}*/
 };
 
 GameClass.prototype.start = function() {
@@ -36,11 +54,3 @@ GameClass.prototype.start = function() {
     //this is the proper way of monitoring system tick in createjs
 	createjs.Ticker.addEventListener('tick', Delegate.create(this,this.tick));
 };
-
-
-=======
-
-    //this is the proper way of monitoring system tick in createjs
-	createjs.Ticker.addEventListener('tick', Delegate.create(this,this.tick));
-};
->>>>>>> 68985ea6199f58a8b21bba9f478b74f4a29bad86
