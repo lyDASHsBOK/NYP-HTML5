6558509b24b4eede133263948ef3d2a76a4185bd
winstonho <dkrox999@hotmail.com>
2014-09-10 11:29:27 +0800

    movement done

--- createjs_games_NYP/FirstGame/js/gameClass.js
+++ createjs_games_NYP/FirstGame/js/gameClass.js
@@ -1,24 +1,29 @@
 
-
-
 function GameClass(stage, imgContainer)
 {
 	
 	console.log("hi!");
     this.stage_ = stage;
 	this.bg = new createjs.Bitmap(imgContainer["imgs/bg.png"]);
-	this.red = new Creep();
-	this.blue = new createjs.Bitmap(imgContainer["imgs/blue.png"]);
+	this.red = new Creep(1);
+	this.blue = new BlueCreep(2);
 
-    this.bg.addEventListener('click', this.onMouseClick);
+   this.bg.addEventListener('click', this.onMouseClick);
+  
+
 }
 
+GameClass.prototype.tick = function(event) {
+	this.red.move();
+	this.blue.move();
+	
+};
+
 GameClass.prototype.loadImage = function() {
-    this.blue.y = 150;
+	this.blue.y = 150;
     this.stage_.addChild(this.bg);
     this.stage_.addChild(this.red);
     this.stage_.addChild(this.blue);
-
 };
 
 GameClass.prototype.onMouseClick = function(e) {
@@ -26,5 +31,6 @@ GameClass.prototype.onMouseClick = function(e) {
 };
 
 GameClass.prototype.start = function() {
-    this.loadImage();
+    this.loadImage();	   
+	this.red.addEventListener('tick', Delegate.create(this, this.tick));
 };
