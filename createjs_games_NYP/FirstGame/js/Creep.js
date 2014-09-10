949325905b3eecfde0ec98f43fa117d2e88371fc
huanzhz <huanzhangrain@gmail.com>
2014-09-10 12:37:03 +0800

    Revert "a3"
    This reverts commit 69d94c0e810f834c54feb0f33fff13b7b0c81e9e.

---
+++ createjs_games_NYP/FirstGame/js/Creep.js
@@ -0,0 +1,28 @@
+/**
+ * Created by Enveesoft.
+ * User: Liu Xinyi
+ * Date: 14-9-10
+ * Time: 上午10:11
+ * Write the description in this section.
+ */
+
+//this function does the inheritance
+BOK.inherits(Creep, createjs.Container);
+
+/**
+ * @constructor
+ * */
+function Creep(speed) {
+    //this line is a must-have in prototype-chain style inheritance
+    //Compare to JAVA this works as super();
+    createjs.Container.call(this);
+
+	this.speed_ = speed;
+	
+    this.addChild(new createjs.Bitmap(imgContainer["imgs/red.png"]));
+}
+
+Creep.prototype.move = function() {
+	//console.log("red!");
+	this.x = this.x + this.speed_; 
+};
---
+++ createjs_games_NYP/FirstGame/js/gameClass.js
@@ -0,0 +1,36 @@
+
+function GameClass(stage, imgContainer)
+{
+	console.log("hi!");
+    this.stage_ = stage;
+	this.bg = new createjs.Bitmap(imgContainer["imgs/bg.png"]);
+	this.red = new Creep(1);
+	this.blue = new createjs.Bitmap(imgContainer["imgs/blue.png"]);
+	this.blue = new BlueCreep(2);
+	
+    this.bg.addEventListener('click', this.onMouseClick);
+
+}
+
+GameClass.prototype.tick = function() {
+	this.red.move();
+	this.blue.move();
+};
+
+GameClass.prototype.loadImage = function() {
+    this.blue.y = 150;
+    this.stage_.addChild(this.bg);
+    this.stage_.addChild(this.red);
+    this.stage_.addChild(this.blue);
+
+};
+
+GameClass.prototype.onMouseClick = function(e) {
+    console.log('stage clicked!');
+};
+
+GameClass.prototype.start = function() {
+    this.loadImage();
+	this.red.addEventListener('tick', Delegate.create(this, this.tick));
+};
+
