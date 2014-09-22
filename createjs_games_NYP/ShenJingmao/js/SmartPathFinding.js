98c179026a83ffca069535cbc8bb128593abe894
刘忻沂 <liuxy@enveesoft.com>
2014-09-22 10:46:24 +0800

    add AI

--- createjs_games_NYP/ShenJingmao/js/SmartPathFinding.js
+++ createjs_games_NYP/ShenJingmao/js/SmartPathFinding.js
@@ -68,8 +68,103 @@ SmartPathFinding.prototype.getSmallerStep = function(value , index) {
 	}
  };
 
+SmartPathFinding.prototype.findPathLy = function(map) {
+    function Node(tile) {
+        this.tile = tile;
+        this.branches = [];
+        this.wayOutBranch = null;
+        this.record = [];
+        this.updateWayOut = function(branch){
+            if(!this.wayOutBranch || this.wayOutBranch.nodes.length > branch.nodes.length){
+                this.wayOutBranch = branch;
+                this.record = [branch];
+            } else if(this.wayOutBranch && this.wayOutBranch.nodes.length == branch.nodes.length) {
+                this.record.push(branch);
+            }
+        };
+
+        this.addBranch = function(branch){
+            this.branches.push(branch);
+        };
+    }
+
+    function Branch() {
+        this.nodes = [];
+        this.isWayOut = false;
+        this.endNode = null;
+        this.addNode = function(node){
+            node.addBranch(this);
+            this.nodes.push(node);
+            this.endNode = node;
+        };
+        this.clone = function(){
+            var b = new Branch();
+            b.nodes = this.nodes.concat();
+            b.isWayOut = this.isWayOut;
+            for(var i=0; i<b.nodes.length; ++i){
+                b.nodes[i].addBranch(b);
+            }
+            return b;
+        };
+        this.markWayOut = function(){
+            this.isWayOut = true;
+            for(var i=0; i<this.nodes.length; ++i){
+                this.nodes[i].updateWayOut(this);
+            }
+        }
+    }
+    function pf(map, node, root){
+        if(UtilCheck.checkWeiZhu(map.clone(), node.tile))
+            return;
+
+        root || (root = node);
+
+        map.markCellColored(node.tile);
+        var surrounding = map.getSurroundingTiles(node.tile);
+        for(var i=0; i<  surrounding.length; ++i){
+            var tile = surrounding[i];
+            if(!map.isCellClicked(tile)) {
+                var subNode = new Node(tile);
+                var branch = node.branches[0].clone();
+                branch.addNode(subNode);
+                if(root.wayOutBranch && branch.nodes.length >= root.wayOutBranch.nodes.length)
+                    break;
+                else if(UtilCheck.outOfBound(tile)){
+                    branch.markWayOut();
+                } else {
+                    pf(map.clone(), subNode, root);
+                }
+            }
+        }
+    }
+
+    var root = new Node(map.catTile);
+    var route = new Branch();
+    route.addNode(root);
+
+    if(UtilCheck.checkWeiZhu(map.clone())) {
+        console.log('trapped');
+    }else {
+        pf(map, root);
+        var direct = 0;
+        var sur = map.getSurroundingTiles(root.tile);
+        var branch = BOK.randArrayItem(root.record);
+        BOK.each(sur, function(tile, index){
+            if(tile == branch.nodes[1].tile){
+                direct = index + 1;
+                return BOK.BREAK;
+            }
+        });
+        console.log((root.wayOutBranch.nodes.length-1)+'@'+direct);
+    }
+};
+
 SmartPathFinding.prototype.findPath = function(map) {
-  
+    var t = new Date().getTime();
+    this.findPathLy(map.clone());
+    console.log('algorithm elapse: '+(new Date().getTime() - t));
+
+    t = new Date().getTime();
   if( ! UtilCheck.outOfBound(map.catTile)){
 		
 		this.smallestValue = 0;
@@ -92,6 +187,8 @@ SmartPathFinding.prototype.findPath = function(map) {
 		  for(var i = 0; i < this.pathList.length; i++ ){
 			this.getSmallerStep(this.pathList[i].step_ , this.pathList[i].directionID_ + 1);
 		  }
+      console.log('Choose: '+this.smallestValue + '@'+this.shortestPathIndex);
+      console.log('algorithm elapse: '+(new Date().getTime() - t));
 
 		 if(this.shortestPathIndex  == this.DIRECTION.LEFT){
 			return this.DIRECTION.LEFT;
@@ -107,7 +204,7 @@ SmartPathFinding.prototype.findPath = function(map) {
 			return this.DIRECTION.BOTTOM_LEFT;
 		}
 	}
-	
+
 	return this.DIRECTION.NO_DIRECTION;
 	  
 };
