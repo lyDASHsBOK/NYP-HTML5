3431e6946e709e7b93f6ab319590448276ee39fd
winstonho <dkrox999@hotmail.com>
2014-09-23 18:11:39 +0800

    more hill add

--- createjs_games_NYP/SuperMario/js/MapModule.js
+++ createjs_games_NYP/SuperMario/js/MapModule.js
@@ -17,7 +17,7 @@ function MapModule(mapRow , mapCol){
 	
 	this.buildLevelOne();
 	
-	this.walkableTile = [-1 ,272 ,274,273 , 375 ];
+	this.walkableTile = [-1 ,272 ,274,273 ,307,306,305, 375 ];
 	
 	
 }
@@ -52,25 +52,19 @@ MapModule.prototype.buildLevelOne =  function (){
 	
 	//green block
 	
-	var temp = 1;
-	for (var row = this.mapHeight - 6; row < this.mapHeight - 2; row++) {
-		for (var col = 112; col <  116 ; col++) {
-			if(col  >=  116 - temp){
-				this.firstLevel[row][col] = 33;
-			 }
-		}
-		temp++;
-	}
+	this.createGreenBlock(112 , this.mapHeight - 6 , 4 , 4, true);
+	this.createGreenBlock(118 , this.mapHeight - 6 , 4 , 4, false);
+	this.createGreenBlock(134 , this.mapHeight - 6 , 4 , 4, false);
 	
-	temp = 4;
-	for (var row = this.mapHeight - 6; row < this.mapHeight - 2; row++) {
-		for (var col = 118; col <  122 ; col++) {
-			if(col + temp < 123){
-				this.firstLevel[row][col] = 33;
-			 }
-		}
-		temp--;
-	}
+	this.createGreenBlock(159 , this.mapHeight - 10 , 8 , 8 , true);
+	this.createGreenBlock(167 , this.mapHeight - 10 , 8 , 1, true);
+	
+
+	this.createGreenBlock(127 , this.mapHeight - 6 , 4 , 4 , true);
+	this.createGreenBlock(131 , this.mapHeight - 6 , 4 , 1 , true);
+	
+	
+	
 	
 	for (var row = this.mapHeight - 3; row < this.mapHeight - 2; row++) {
 		for (var col = 116; col <  118 ; col++) {
@@ -80,46 +74,7 @@ MapModule.prototype.buildLevelOne =  function (){
 	
 	
 	
-	temp = 2;
-	for (var row = this.mapHeight - 6; row < this.mapHeight - 2; row++) {
-		for (var col = 126; col <  132 ; col++) {
-			if(col  >=  132 - temp){
-				this.firstLevel[row][col] = 33;
-			 }
-		}
-		temp++;
-	}
 	
-	
-	temp = 4;
-	for (var row = this.mapHeight - 6; row < this.mapHeight - 2; row++) {
-		for (var col = 134; col <  138 ; col++) {
-			if(col + temp < 139){
-				this.firstLevel[row][col] = 33;
-			 }
-		}
-		temp--;
-	}
-	
-	temp = 1;
-	for (var row = this.mapHeight - 10; row < this.mapHeight -2; row++) {
-		for (var col = 159; col <  168 ; col++) {
-			if(col  >=  168 - temp){
-				this.firstLevel[row][col] = 33;
-			}
-		}
-		temp++;
-	}
-	
-	temp = 1;
-	for (var row = this.mapHeight - 10; row < this.mapHeight -2; row++) {
-		for (var col = 159; col <  168 ; col++) {
-			if(col  >=  168 - temp){
-				this.firstLevel[row][col] = 33;
-			}
-		}
-		temp++;
-	}
 	
 	this.firstLevel[this.mapHeight -3][176] = 33;
 	
@@ -141,11 +96,24 @@ MapModule.prototype.buildLevelOne =  function (){
 	this.createVPiple(46 , this.mapHeight -3 , 4 );
 	this.createVPiple(56 , this.mapHeight -3 , 4 );
 	
+	
 	//hill
-	temp = 1;
-	for (var row = this.mapHeight - 4; row < this.mapHeight -2; row++) {
-		for (var col = 0; col <  3 ; col++) {
-			if(col  ==  3 - temp){
+	this.createHill(0,this.mapHeight - 4,2, 5);
+	this.createHill(15,this.mapHeight - 3,1, 5);
+	this.createHill(47,this.mapHeight - 4,2, 5);
+	this.createHill(61,this.mapHeight - 3,1, 5);
+	
+	this.createHill(169,this.mapHeight - 4,2, 5);
+};
+
+MapModule.prototype.createHill = function(startX , startY , height,width) {
+	//hill
+	var middleX = Math.floor( (startX + startX + width) / 2 ) + 1;
+	
+	var temp = 1;
+	for (var row = startY; row < startY + height; row++) {
+		for (var col = startX ; col <  middleX ; col++) {
+			if(col  ==  middleX - temp){
 				this.firstLevel[row][col] = 272;
 			}
 		}
@@ -153,15 +121,56 @@ MapModule.prototype.buildLevelOne =  function (){
 	}
 	
 	temp = 2;
-	for (var row = this.mapHeight - 4; row < this.mapHeight -2; row++) {
-		for (var col = 3; col <  6 ; col++) {
-			if(col  ==  6 - temp){
+	for (var row = startY; row < startY + height; row++) {
+		for (var col = middleX; col <  startX + width +1 ; col++) {
+			if(col  ==  startX + width +1 - temp){
 				this.firstLevel[row][col] = 274;
 			}
 		}
 		temp--;
 	}
-	this.firstLevel[this.mapHeight -5][3] = 273;
+	
+	for (var row = startY; row < startY + height; row++) {
+		for (var col = startX + 2 ; col <  startX + width ; col++) {
+			if(row == startY +1 || col  == middleX){
+				this.firstLevel[row][col] = 305;
+			}
+			if( col  == middleX && row != startY){
+				this.firstLevel[row][col] = 306;
+			}
+		}
+	}
+	this.firstLevel[startY -1][middleX] = 273;
+};
+
+MapModule.prototype.createGreenBlock = function(startX , startY , height,width, isLeft) {
+
+	if(isLeft){
+	var temp = 1;
+		for (var row = startY; row < startY + height; row++) {
+			for (var col = startX; col < startX + width ; col++) {
+				if(col  >=  startX + width - temp){
+					this.firstLevel[row][col] = 33;
+				 }
+			}
+			temp++;
+		}
+	}
+	else{
+		var temp = 4;
+		
+		for (var row = startY; row < startY + height; row++) {
+			for (var col = startX; col <  startX + width ; col++) {
+				if(col + temp < startX + width +1){
+					this.firstLevel[row][col] = 33;
+				 }
+			}
+			temp--;
+		}
+			
+	}
+	
+	
 };
 
 MapModule.prototype.createVPiple =  function (startX , startY , height ){
